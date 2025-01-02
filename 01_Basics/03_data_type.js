f9456b855ebe4e67252f2381f544a50044c78a12
Samip-Acharya <bygkttan90level@gmail.com>
2025-01-02 16:25:59 +0000

    data type conversion

---
+++ 01_Basics/03_data_type.js
@@ -0,0 +1,8 @@
+let age = 67 //Number
+let state; //Undefined
+let Name = "Samip" //String
+let condn = false
+//BIGINT
+//BOOLEAN 
+//NULL -> Object
+console.log(typeof undefined)
---
+++ 01_Basics/04_data_type_conversion.js
@@ -0,0 +1,9 @@
+// let num = "45h"
+// console.log(typeof(num))
+// let numVal = Number(num)
+// console.log(typeof(numVal))
+// console.log(numVal);
+ 
+// let stateNum = "aaaa";
+// state = Boolean(stateNum)
+// console.log(state)
