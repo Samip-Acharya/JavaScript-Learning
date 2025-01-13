3659869ccd33bf47e4e74313f7fc8db9b3afbf74
Samip-Acharya <bygkttan90level@gmail.com>
2025-01-13 17:27:18 +0000

    array

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,20 @@
+// const arr = [1,2,3,4,6,7]
+// console.log(arr[2])
+// arr.unshift(10)
+// console.log(arr)
+
+// // arr.shift()
+// // arr.push(0)
+// arr.pop()
+// console.log(arr)
+
+const arr = new Array(0,1,2,3,4)
+// console.log(arr.join(" "))
+
+// const newArr = arr.slice(0,4)
+// console.log(arr)
+// console.log(newArr)
+
+// const newArr = arr.splice(0,4)
+// console.log(arr)
+// console.log(newArr)
